5b299891e7d915fd87164f9643a7c701bbd3ffaf
Nazz78 <jernej.vidmar@vidmarboehm.com>
2013-10-19 20:44:39 +0200

    su-tunnel for NetBeans added.
    This is an extended version of Noel's original script, which was made
    since I could not find a way to run the script with the currently opened
    file. What it does? It checks which .rb script (in specified folders)
    was changed last and it then runs it.
    
    If someone has a better way, please let me know!

---
+++ tunnel_ide_for_netbeans.rb
@@ -0,0 +1,82 @@
+# This is an extension of Noel Warr's su-tunnel script, which works on NetBeans.
+# 
+# You need to have tunnel_skp.rb also installed in your Skp/Plugins directory.
+# 
+# It is a quick and dirty ruby script to load last modified .rb file to SketchUp.
+# This script was created as I have not found out a way to send currently
+# focused file directly. So what it does it first looks up which is the last
+# modified (saved) file and then executes based on Noel's script.
+#
+# If you know how to pass the file that is focused in NetBeans directly to 
+# tunnel script, please let us know!
+#
+# NetBeans setup:
+# Go to your project Properties, under Categories (on left side) select Run.
+# Set this script as a Main Script and this should be it.
+# You run it by clicking on Run Main Project icon or by pressing F6.
+
+require 'find'
+
+# Here you define where your .rb files reside. You can specify the full path
+# if it causes you some problems.
+dirs = Dir['../main/', '../tests/']
+# Do not include this folders in your search for the last modified date.
+excludes = [".hg","ext","env","i18n","license"]
+# Check only Ruby files
+included_files = ['.rb']
+
+# Set the variable to hold last modified filepath
+last_modified_file = String.new
+# Go way back in time...
+last_modification_time = Time.utc(2000,1,1)
+# now search for all the ruby files
+for dir in dirs
+	folder = ''
+	Find.find(dir) do |path|
+		if FileTest.directory?(path)
+			if excludes.include?(File.basename(path))
+				# Don't look any further into this directory.
+				Find.prune
+			else
+				next
+			end
+		else
+			if included_files.include?(File.extname(path))
+				# get last modification date of the file...
+				file = File.open path, 'r'
+				modification_time = file.mtime.utc
+				#				puts modification_time
+				file.close
+
+				# and set it as last modified if appropriate
+				if modification_time > last_modification_time
+					last_modified_file = File.expand_path(path)
+					last_modification_time = modification_time
+				end
+			end
+		end
+	end
+end
+
+# Now we go to Noel's script
+require 'socket'
+HOST = "127.0.0.1"
+PORT = 1517
+
+def kill_server
+	socket = TCPSocket.open(HOST,PORT)
+	socket.puts
+	socket.close
+	sleep 2
+end
+
+begin
+	server = TCPServer.new(HOST, PORT)
+rescue 
+	kill_server
+	server = TCPServer.new(HOST, PORT)
+end
+client = server.accept
+client.puts "LOAD:" + last_modified_file
+puts client.read
+client.close
