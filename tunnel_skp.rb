29026c187fbdc39c4d3d86fb4ad7e8f5f5b67af5
unknown <tthomas2@tthomas-westbc1.am.trimblecorp.net>
2013-10-22 15:15:12 +0200

    Converted class variables to instance variables.

--- tunnel_skp.rb
+++ tunnel_skp.rb
@@ -5,30 +5,31 @@
 # logs the output and feeds it back through the socket
 module Tunnel
 
-  HOST             = '127.0.0.1'
-  PORT             = 1517
-  @@connected      = false
-  @@running_script = false
-  @@log            = Array.new
+  HOST            = '127.0.0.1'.freeze
+  PORT            = 1517
 
-  def self.connected?;      @@connected;      end;
-  def self.running_script?; @@running_script; end;
-  def self.log;             @@log;            end;
+  @connected      = false
+  @running_script = false
+  @log            = Array.new
+
+  def self.connected?;      @connected;      end;
+  def self.running_script?; @running_script; end;
+  def self.log;             @log;            end;
 
   def self.connect
-    if !@@connected
-      @@connected = true
+    if !@connected
+      @connected = true
       SKSocket.connect(HOST, PORT)
       SKSocket.add_socket_listener { |msg|
         if msg[0..4] == 'LOAD:'
           begin
             path = msg[5..-1]
-            @@running_script = true
+            @running_script = true
             load path
-            @@running_script = false
+            @running_script = false
             SKSocket.write(Tunnel.log.join)
           rescue Exception => e
-            @@running_script = false
+            @running_script = false
             SKSocket.write(
               Tunnel.log.join <<
               e.message <<
@@ -40,22 +41,22 @@ module Tunnel
         end
         if msg != 'Connection established'
           SKSocket.disconnect
-          @@connected = false
+          @connected = false
         end
       }
     end
   end
 
   def self.start
-    @@timer = UI.start_timer(1.4, true) {
+    @timer = UI.start_timer(1.4, true) {
       Tunnel.connect
     }
     Tunnel
   end
 
   def self.stop
-    UI.stop_timer(@@timer)
-    @@timer = true
+    UI.stop_timer(@timer)
+    @timer = true
     Tunnel
   end
 
@@ -93,7 +94,7 @@ class TunnelProxy
     raise NotImplementedError
   end
 
-end
+end # class
 
 proxy = TunnelProxy.new
 $stdout = proxy
